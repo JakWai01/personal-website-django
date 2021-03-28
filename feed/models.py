f2022e68c4219ffd73993d46a8008eda15b1d69d
JakWai01 <jakob.waibel@web.de>
2021-03-28 12:12:15 +0200

    feat: Create models

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Blogpost(models.Model):
+    title = models.CharField(max_length = 255)
+    date = models.DateField(auto_created=True)
+    abstract = models.CharField(max_length = 1023)
+    text = models.CharField(max_length = 8191)
+
+class Project():
+    title = models.CharField(max_length = 255)
+    date = models.DateField(auto_created=True)
+    abstract = models.CharField(max_length = 1023)
