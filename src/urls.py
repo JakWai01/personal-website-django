43e0173d97c47a3f9b1e70184bf51d9626f271a7
JakWai01 <jakob.waibel@web.de>
2021-03-28 18:20:53 +0200

    feat: Filter feed when clicking on either blog or projects

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, blog, projects
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('blog', blog, name='blog'),
+    path('projects', projects, name='projects')
 ]
--- src/views.py
+++ src/views.py
@@ -4,6 +4,13 @@ from .models import Content
 
 # Create your views here.
 def index(request):
-
     content = Content.objects.all().order_by('date').reverse()
+    return render(request, 'src/feed.html', {'content': content})
+
+def blog(request):
+    content = Content.objects.filter(is_blog=True)
+    return render(request, 'src/feed.html', {'content': content})
+
+def projects(request):
+    content = Content.objects.filter(is_blog=False)
     return render(request, 'src/feed.html', {'content': content})
