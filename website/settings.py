22c383ab2b07ea7785bb9359fe2ad2cd0a4fb36d
JakWai01 <jakob.waibel@web.de>
2021-04-02 14:40:38 +0200

    feat: Add static folder

--- website/settings.py
+++ website/settings.py
@@ -123,10 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
