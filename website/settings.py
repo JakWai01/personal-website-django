8ed01c9c6c44e505d38cad4b40e5d4d8a0ea5bf6
JakWai01 <jakob.waibel@web.de>
2021-04-02 14:04:44 +0200

    feat: Add configuration files for deployment

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'w5c8v2w)3*6)or!0y6w8==(glb7%fw&b1)vlb%er#^^iltezz_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "jakobwaibel.herokuapp.com", "localhost", "127.0.0.1"
+]
 
 
 # Application definition
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -118,4 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
